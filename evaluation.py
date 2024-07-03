b4a98851c22066e7a4fa602b835badbe377f2482
Daan Van Wesenbeeck <daanv@pinac40.cs.kuleuven.be>
2024-07-03 10:09:37 +0200

    add macro-averaged precision, recall and f1-score

--- evaluation.py
+++ evaluation.py
@@ -3,11 +3,7 @@ import time
 import pandas as pd
 import scipy.optimize
 
-def evaluate(gt, discovered_sets):
-    mm = match_matrix(discovered_sets, gt)
-    return f1_score(mm)
-    
-
+## MATCH MATRIX
 def overlap_union_ratio(s, e, s_gt, e_gt):
     return max(0, (min(e, e_gt) - max(s, s_gt)) / (max(e, e_gt) - min(s, s_gt)))
 
@@ -16,24 +12,24 @@ def match_matrix(gt, discovered_sets, threshold=0.5):
     assert (threshold >= 0.5)
     assert gt
         
-    n, m = len(gt), len(discovered_sets)
-    column_names = np.arange(1, m+1)
+    g, d = len(gt), len(discovered_sets)
+    column_names = np.arange(1, d+1)
     if type(gt) is list:
-        row_names = np.arange(1, n+1)
+        row_names = np.arange(1, g+1)
         gt_sets   = gt
     elif type(gt) is dict:
         row_names = np.array(list(gt.keys())) 
         gt_sets   = list(gt.values())
     
     if not discovered_sets:
-        # only return missed discovery column
-        match_matrix = np.zeros((n+1, 1), dtype=int)
+        # only return missed detection column
+        match_matrix = np.zeros((g+1, 1), dtype=int)
         match_matrix[:-1, 0] = [len(gt_set) for gt_set in gt_sets]
         match_matrix[-1, 0]  = 0
         return match_matrix, row_names, np.array([])
     
     # inner match matrix
-    mm = np.zeros((n, m), dtype=int)
+    mm = np.zeros((g, d), dtype=int)
     for i, gt_set in enumerate(gt_sets):
         for (s_gt, e_gt) in gt_set:
             # find the best match in any motif set, greater than threshold
@@ -51,8 +47,9 @@ def match_matrix(gt, discovered_sets, threshold=0.5):
     
     # r and c are of length min(n, m)
     r, c = scipy.optimize.linear_sum_assignment(mm, maximize=True)
-    r_perm = np.hstack((r, np.setdiff1d(range(n), r))) 
-    c_perm = np.hstack((c, np.setdiff1d(range(m), c)))
+    
+    r_perm = np.hstack((r, np.setdiff1d(range(g), r))) 
+    c_perm = np.hstack((c, np.setdiff1d(range(d), c)))
     
     # compute number of missed detections
     md = np.array([len(gt_set) for gt_set in gt_sets])
@@ -71,38 +68,72 @@ def match_matrix(gt, discovered_sets, threshold=0.5):
     md = md[r_perm]
     
     md = np.expand_dims(md, axis=0).T
-    match_matrix = np.block([
+    M = np.block([
         [mm, md],
         [fd, 0 ] 
     ])
-    return match_matrix, row_names[r_perm], column_names[c_perm]
+    return M, row_names[r_perm], column_names[c_perm]
 
 
-def recall(match_matrix):
-    n, m = match_matrix.shape[0] - 1, match_matrix.shape[1] - 1
-    diag = np.diag(match_matrix[:min(n, m),:min(n, m)])
-    return np.sum(diag) / np.sum(match_matrix[:n, :])
-
-def precision(match_matrix, penalize_additional=False):
-    n, m = match_matrix.shape[0] - 1, match_matrix.shape[1] - 1
-    # no motif sets found
-    if m == 0:
+## MICRO-AVERAGED METRICS
+def micro_averaged_recall(match_matrix):
+    g, d = match_matrix.shape[0] - 1, match_matrix.shape[1] - 1
+    if d == 0:
         return 0.0
-    diag = np.diag(match_matrix[:min(n, m),:min(n, m)])
+    m = min(g, d)
+    diag = np.diag(match_matrix[:m, :m])
+    return np.sum(diag) / np.sum(match_matrix[:g, :])
+
+
+def micro_averaged_precision(match_matrix, penalize_additional=False):
+    g, d = match_matrix.shape[0] - 1, match_matrix.shape[1] - 1
+    m = min(g, d)
+
+    diag = np.diag(match_matrix[:m, :m])
     
     if penalize_additional:
+        return np.sum(diag) / np.sum(match_matrix[:, :d])
+    else:
         return np.sum(diag) / np.sum(match_matrix[:, :m])
-    else:
-        return np.sum(diag) / np.sum(match_matrix[:, :min(n, m)])
 
-def f1_score(match_matrix):
-    prec = precision(match_matrix)
-    rec  = recall(match_matrix)
-    if not (prec == 0 and rec == 0):
-        return 2 * rec * prec / (prec + rec)
-    else:
-        return 0
+def micro_averaged_f1(match_matrix, penalize_additional=False):
+    p = micro_averaged_precision(match_matrix, penalize_additional=penalize_additional)
+    r  = micro_averaged_recall(match_matrix)
+    return 2 * r * p / (p + r)
 
+## MACRO-AVERAGED METRICS
+def macro_averaged_precision(match_matrix, penalize_additional=False):
+    g, d = match_matrix.shape[0] - 1, match_matrix.shape[1] - 1
+    m = min(g, d)    
+
+    tps = np.diag(match_matrix[:m, :m])
+    ps = np.divide(tps, np.sum(match_matrix[:, :m], axis=0))
+    p = np.sum(ps) / d if penalize_additional else np.sum(ps) / m
+    return p
+
+def macro_averaged_recall(match_matrix):
+    g, d = match_matrix.shape[0] - 1, match_matrix.shape[1] - 1
+    m = min(g, d)
+
+    tps = np.diag(match_matrix[:m, :m])
+    rs = np.divide(tps, np.sum(match_matrix[:m, :], axis=1))
+    r  = np.sum(rs) / g
+    return r
+
+def macro_averaged_f1(match_matrix):
+    g, d = match_matrix.shape[0] - 1, match_matrix.shape[1] - 1
+    assert d >= g
+    m = min(g, d)
+    
+    tps = np.diag(match_matrix[:m, :m])
+    rs = np.divide(tps, np.sum(match_matrix[:m, :], axis=1))
+    ps = np.divide(tps, np.sum(match_matrix[:, :m], axis=0))
+    f1s = 2 * np.divide(np.multiply(rs, ps), (rs + ps))
+    f = np.sum(f1s) / m
+    return f
+
+
+## VISUALIZATION
 def pretty_print_match_matrix(match_matrix, row_names, col_names):
     from tabulate import tabulate
     table, row_names, col_names = match_matrix.tolist(), row_names.tolist(), col_names.tolist()
